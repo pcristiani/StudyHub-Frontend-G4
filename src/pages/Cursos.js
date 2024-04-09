ed94678daadbe1bc17b2c6a1f138db40eaa30013
Sebastiangonlai <58099324+Sebastiangonlai@users.noreply.github.com>
2024-04-08 23:09:46 -0300

    jspdf

--- src/pages/Cursos.js
+++ src/pages/Cursos.js
@@ -7,6 +7,8 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { Graphviz } from 'graphviz-react';
 import { PARAMETERS, URL_BACK } from '../util/constants'
+import { jsPDF } from "jspdf";
+
 
 async function getUsers() {
     console.log("URL: ", URL_BACK.courseRelations);
@@ -18,12 +20,46 @@ async function getUsers() {
         },
     }
     );
+
     if (responses.ok) {
         return await responses.text();
     }
 }
 
+const downloadPDF = (event) => {
+    var doc = new jsPDF();
+
+    doc.setTextColor(100);
+    doc.text(`PRUEBA`, 20, 20);
+
+    doc.setTextColor(150);
+    doc.text("This is light gray.", 20, 30);
+
+    doc.setTextColor(255, 0, 0);
+    doc.text("This is red.", 20, 40);
+
+    doc.setTextColor(0, 255, 0);
+    doc.text("This is green.", 20, 50);
+
+    // doc.setTextColor(0, 0, 255);
+    // doc.text("This is blue.", 20, 60);
+
+    // doc.setTextColor("red");
+    // doc.text("This is red.", 60, 40);
+
+    // doc.setTextColor("green");
+    // doc.text("This is green.", 60, 50);
+
+    // doc.setTextColor("blue");
+    // doc.text("This is blue.", 60, 60);
+    // const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
+}
+
 const Cursos = () => {
+
     const [strJwt, setStrJwt] = useState(null);
 
     useEffect(() => {
@@ -36,6 +72,7 @@ const Cursos = () => {
     return (
         <CssVarsProvider>
             <Container component="main" maxWidth="xs">
+                <button onClick={downloadPDF}> Descargar PDF </button>
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '22px' }}>
                     <Typography className="text-dark focus-ring-primary" component="h1" >
                         Cursos
@@ -44,7 +81,7 @@ const Cursos = () => {
                     <Graphviz dot={strJwt} options={{ width: 1000, height: 700 }} />
                 </Box>
             </Container>
-        </CssVarsProvider>
+        </CssVarsProvider >
 
     );
 }
