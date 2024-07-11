a07a892259098c981595365ac9a346ab3004b0a7
Pedro Aldama <PedroAldama47@gmail.com>
2024-07-11 20:52:41 -0300

    Agregado hook para obtener token de URL al recuperar contraseña

--- src/components/login/ForgotPassword.js
+++ src/components/login/ForgotPassword.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import swal from 'sweetalert';
 import Input from '@mui/joy/Input';
@@ -9,142 +8,145 @@ import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
 import Container from '@mui/joy/Container';
 import Card from '@mui/joy/Card';
-
 import { PARAMETERS, URL_BACK, redirigir, URI_FRONT } from '../../services/util/constants'
 import { Divider, Sheet } from '@mui/joy';
 import FormControl from '@mui/joy/FormControl';
 import Stack from '@mui/joy/Stack';
 
-
-// olvido-contrasenia
 const defaultTheme = createTheme();
 
 const ForgotPassword = () => {
-	let params = new URLSearchParams(window.location.search);
-	let token = params.get('token');
-	const redirectHome = URI_FRONT.homeUri;
+  const [token, setToken] = useState(null);
 
-	async function emailValue(email) {
-		let response = await fetch(URL_BACK.forgotPassword, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${PARAMETERS.accessToken}`,
-			},
-			body: email
-		})
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const tokenParam = params.get('token');
+    if (tokenParam) {
+      setToken(tokenParam);
+    }
+  }, []);
 
-		if (response.ok) {
-			swal({
-				title: "Email de recuperacion su correo electronico.\n\n",
-				icon: "success",
-				position: "center",
-				timer: 4000
-			});
+  const redirectHome = URI_FRONT.homeUri;
 
-			redirigir(redirectHome);
-		} else {
-			swal("¡Advertencia!", 'Email invalido', "error", {
-				timer: 3000
-			});
-		}
-	}
+  const emailValue = async (email) => {
+    let response = await fetch(URL_BACK.forgotPassword, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${PARAMETERS.accessToken}`,
+      },
+      body: JSON.stringify({ email })
+    });
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		let email = data.get('email');
-		emailValue(email);
-	}
+    if (response.ok) {
+      swal({
+        title: "Email de recuperación enviado a su correo electrónico.",
+        icon: "success",
+        position: "center",
+        timer: 4000
+      });
 
+      redirigir(redirectHome);
+    } else {
+      swal("¡Advertencia!", 'Email inválido', "error", {
+        timer: 3000
+      });
+    }
+  }
 
-	async function recuperarPassword(token, newPassword) {
-		const redirectLogin = URI_FRONT.loginUri;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    let email = data.get('email');
+    emailValue(email);
+  }
 
-		let body = { "token": token, "newPassword": newPassword };
-		let response = await fetch(URL_BACK.recuperarPassword, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${PARAMETERS.accessToken}`,
-			},
-			body: JSON.stringify(body)
-		})
+  const recuperarPassword = async (token, newPassword) => {
+    const redirectLogin = URI_FRONT.loginUri;
+    let body = { "token": token, "newPassword": newPassword };
+    let response = await fetch(URL_BACK.recuperarPassword, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${PARAMETERS.accessToken}`,
+      },
+      body: JSON.stringify(body)
+    });
 
-		if (response.ok) {
-			swal({
-				title: "Se actualizo correctamente su contraseña.\n\n",
-				icon: "success",
-				position: "center",
-				timer: 3000
-			});
-			redirigir(redirectLogin);
-		} else {
-			swal("¡Advertencia!", 'Email invalido', "error", {
-				timer: 3000
-			});
-		}
-	}
+    if (response.ok) {
+      swal({
+        title: "Se actualizó correctamente su contraseña.",
+        icon: "success",
+        position: "center",
+        timer: 3000
+      });
+      redirigir(redirectLogin);
+    } else {
+      swal("¡Advertencia!", 'Email inválido', "error", {
+        timer: 3000
+      });
+    }
+  }
 
-	const resetPassword = (event) => {
-		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		let newPass = data.get('newPass');
-		let confirmNewPass = data.get('confirmNewPass');
+  const resetPassword = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    let newPass = data.get('newPass');
+    let confirmNewPass = data.get('confirmNewPass');
 
-		if (newPass === confirmNewPass) {
-			recuperarPassword(token, confirmNewPass);
-		} else {
-			swal("¡Advertencia!", 'La contraseña no coincide', "error", {
-				timer: 3000
-			});
-		}
-	}
+    if (newPass === confirmNewPass) {
+      recuperarPassword(token, confirmNewPass);
+    } else {
+      swal("¡Advertencia!", 'La contraseña no coincide', "error", {
+        timer: 3000
+      });
+    }
+  }
 
-	return (
-		<Sheet>
-			<Container component="main" maxWidth="xs" sx={{ marginBlockEnd: 12 }}>
-				<Box sx={{ marginTop: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-					{(!token) ?
-						<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 0 }}>
-							<Card sx={{ display: 'flex', alignSelf: 'center', }}>
-								<Box sx={{ margin: 0.6, alignSelf: 'center' }}>
-									<Typography sx={{ textAlign: 'center' }} variant="plain" color="primary" noWrap>Verificar su identidad</Typography>
-								</Box>
-								<Divider />
-								<Stack direction="column" sx={{ display: { xs: 'flex', md: 'flex' }, alignSelf: 'center' }}>
-									<FormControl sx={{ display: { sm: 'flex', md: 'flex', width: '300px' }, gap: 0.8 }}>
-										<Input size="sm" id="email" name="email" placeholder="Ingresar correo de recuperacion" autoComplete="text" autoFocus required />
-									</FormControl>
-									<Stack direction="row" spacing={0.8} sx={{ marginTop: 1, justifyContent: 'right', zIndex: '1000' }}>
-										<Button type="submit" fullWidth sx={{ mt: 1, mb: 3, border: 0.01, borderColor: '#3d3d3d' }} variant="soft">Siguiente</Button>
-									</Stack>
-								</Stack>
-							</Card>
-						</Box>
-						:
-						<Box component="form" onSubmit={resetPassword} noValidate sx={{ mt: 0 }}>
-							<Card sx={{ display: 'flex', alignSelf: 'center', }}>
-								<Box sx={{ margin: 0.6, alignSelf: 'center' }}>
-									<Typography sx={{ textAlign: 'center' }} variant="plain" color="primary" noWrap>Restablezca la contraseña</Typography>
-								</Box>
-								<Divider />
-								<Stack direction="column" sx={{ display: { xs: 'flex', md: 'flex' }, alignSelf: 'center' }}>
-									<FormControl sx={{ display: { sm: 'flex', md: 'flex', width: '320px' }, gap: 1 }}>
-										<Input size="sm" name="newPass" placeholder="Nueva contraseña" type="password" id="newPass" autoComplete="current-password" required />
-										<Input size="sm" name="confirmNewPass" placeholder="Confirmar nueva contraseña" type="password" id="confirmNewPass" autoComplete="current-password" required />
-									</FormControl>
-									<Stack direction="row" spacing={1} sx={{ marginTop: 2, justifyContent: 'right' }}>
-										<Button type="submit" fullWidth sx={{ mt: 1, mb: 3, border: 0.01, borderColor: '#3d3d3d' }} variant="soft">Cambiar contraseña</Button>
-										<Button variant="outlined" fullWidth color="neutral" component={Link} to="/">Cancelar</Button>
-									</Stack>
-								</Stack>
-							</Card>
-						</Box>
-					}
-				</Box>
-			</Container>
-		</Sheet >
-	);
+  return (
+    <Sheet>
+      <Container component="main" maxWidth="xs" sx={{ marginBlockEnd: 12 }}>
+        <Box sx={{ marginTop: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {(!token) ?
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 0 }}>
+              <Card sx={{ display: 'flex', alignSelf: 'center', }}>
+                <Box sx={{ margin: 0.6, alignSelf: 'center' }}>
+                  <Typography sx={{ textAlign: 'center' }} variant="plain" color="primary" noWrap>Verificar su identidad</Typography>
+                </Box>
+                <Divider />
+                <Stack direction="column" sx={{ display: { xs: 'flex', md: 'flex' }, alignSelf: 'center' }}>
+                  <FormControl sx={{ display: { sm: 'flex', md: 'flex', width: '300px' }, gap: 0.8 }}>
+                    <Input size="sm" id="email" name="email" placeholder="Ingresar correo de recuperación" autoComplete="text" autoFocus required />
+                  </FormControl>
+                  <Stack direction="row" spacing={0.8} sx={{ marginTop: 1, justifyContent: 'right', zIndex: '1000' }}>
+                    <Button type="submit" fullWidth sx={{ mt: 1, mb: 3, border: 0.01, borderColor: '#3d3d3d' }} variant="soft">Siguiente</Button>
+                  </Stack>
+                </Stack>
+              </Card>
+            </Box>
+            :
+            <Box component="form" onSubmit={resetPassword} noValidate sx={{ mt: 0 }}>
+              <Card sx={{ display: 'flex', alignSelf: 'center', }}>
+                <Box sx={{ margin: 0.6, alignSelf: 'center' }}>
+                  <Typography sx={{ textAlign: 'center' }} variant="plain" color="primary" noWrap>Restablezca la contraseña</Typography>
+                </Box>
+                <Divider />
+                <Stack direction="column" sx={{ display: { xs: 'flex', md: 'flex' }, alignSelf: 'center' }}>
+                  <FormControl sx={{ display: { sm: 'flex', md: 'flex', width: '320px' }, gap: 1 }}>
+                    <Input size="sm" name="newPass" placeholder="Nueva contraseña" type="password" id="newPass" autoComplete="current-password" required />
+                    <Input size="sm" name="confirmNewPass" placeholder="Confirmar nueva contraseña" type="password" id="confirmNewPass" autoComplete="current-password" required />
+                  </FormControl>
+                  <Stack direction="row" spacing={1} sx={{ marginTop: 2, justifyContent: 'right' }}>
+                    <Button type="submit" fullWidth sx={{ mt: 1, mb: 3, border: 0.01, borderColor: '#3d3d3d' }} variant="soft">Cambiar contraseña</Button>
+                    <Button variant="outlined" fullWidth color="neutral" component={Link} to="/">Cancelar</Button>
+                  </Stack>
+                </Stack>
+              </Card>
+            </Box>
+          }
+        </Box>
+      </Container>
+    </Sheet>
+  );
 }
 export default ForgotPassword;
